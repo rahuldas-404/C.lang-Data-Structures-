9c77f4357d3040533c1e3d4593157502303c9164
Rahul Kumar Das <98277636+rahuldas-404@users.noreply.github.com>
2022-07-10 22:47:29 +0530

    Update Normal_syntex.c

--- Array/Normal_syntex.c
+++ Array/Normal_syntex.c
@@ -1,74 +1,37 @@
+// Program to take 5 values from the user and store them in an array
+// Print the elements stored in the array
 #include <stdio.h>
-#include <stdlib.h>
-struct node{
-    int data;
-    struct node *next;
-};
 
+int main() {
+  int values[5];
 
-void count(struct node*head){
-    int count=0;
-    if(head==NULL){
-        printf("empty");
-    }
-    struct node*ptr=NULL;
-    ptr=head;
-    while(ptr!=NULL){
-        count++;
-        ptr=ptr->next;
-    }
-    printf("no. of nodes is %d\n",count);
+  printf("Enter 5 integers: ");
+
+  // taking input and storing it in an array
+  for(int i = 0; i < 5; ++i) {
+     scanf("%d", &values[i]);
+  }
+
+  printf("Displaying integers: ");
+
+  // printing elements of an array
+  for(int i = 0; i < 5; ++i) {
+     printf("%d\n", values[i]);
+  }
+  return 0;
 }
 
-void print(struct node*head)
-{
-    struct node*ptr;
-    ptr=head;
-    while(ptr!=NULL){
-        printf("data is %d\n",ptr->data);
-        ptr=ptr->next;
-    }
-}
 
-void add(struct node*head, int data){
-    struct node *ptr, *temp;
-    ptr=head;
-    
-    temp=(struct node*)malloc(sizeof(struct node));
-    temp->data=data;
-    temp->next=NULL;
-    
-    while(ptr->next!=NULL){
-        ptr=ptr->next;
-    }
-    ptr->next=temp;
-}
 
-int main()
-{
-    struct node *head=NULL;
-    head=(struct node*)malloc(sizeof(struct node));
-    head->data=76;
-    head->next=NULL;
-   
-   struct node *temp=(struct node*)malloc(sizeof(struct node));
-    temp->data=65;
-    temp->next=NULL;
-    head->next=temp;
-    
-    temp=(struct node*)malloc(sizeof(struct node));
-    temp->data=8;
-    temp->next=NULL;
-    head->next->next=temp;
-    
-    int a;
-    printf("enter the number want to add :");
-    scanf("%d",&a);
-    add(head,a);
-    
-    print(head);
-    
-    count(head);
-    
-    return 0;
-}
+
+/*output
+Enter 5 integers: 1
+-3
+34
+0
+3
+Displaying integers: 1
+-3
+34
+0
+3
